869dfe57d4743cd8a9e2d017a2ab8c12b4709514
charles <charles_hinolan@dlsu.edu.ph>
2024-04-02 17:25:01 +0800

    added password change tests from MCO4

--- scripts/accounts.test.js
+++ scripts/accounts.test.js
@@ -7,6 +7,9 @@ const adminController = require('../controllers/admin-controller.js');
 const authController = require('../controllers/auth-controller.js');
 const employeeController = require('../controllers/employee-controller.js');
 const User = require('../models/User');
+const Admin = require('../models/Admin');
+const Employee = require('../models/Employee');
+const bCrypt = require('bcrypt');
 
 /* ACCOUNT MANAGEMENT TEST SUITE */
 
@@ -83,4 +86,121 @@ describe("ACCOUNT MANAGEMENT TEST SUITE\nLogin Functionality", () => {
     });
   });
 
-  //Password Changing
+//Password Changing for users (negative)
+describe("Password Changing for users (negative)", () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+    it("postSettings successfully sends a 403 response because customer did not enter correct password", async   () => {
+      const req = {
+        session: {
+          logged_in: {
+            user: {
+              userID: '5f8614a2f796ac1c7e62af94' 
+            },
+            type: "customer"
+          }
+        },
+        body:{
+          old_password: 'apassword',
+          new_password: 'newPassword',
+          customer_id: '5f8614a2f796ac1c7e62af94',
+          fname: 'Juan',
+          lname: 'Carlos',
+          email: 'yanyan@gmail.com',
+          contact: '09123456789',
+        }
+      }
+
+      const res = { 
+        sendStatus: jest.fn(),
+        status: jest.fn(),
+        send: jest.fn()
+      }
+
+      jest.spyOn(User, 'findOne').mockResolvedValue({_id: req.body.customer_id}, 'password');
+      
+      bCrypt.compare.mockResolvedValue(false);
+      res.status.mockImplementation(() => res);
+
+
+      await controller.postSettings(req, res);
+      expect(res.status).toHaveBeenCalledWith(403);
+
+
+    });
+
+    it("postAdminSettings successfully sends a 403 response because admin did not enter correct password", async   () => {
+    
+      const req = {
+        session: {
+          logged_in: {
+            user: {
+              username: 'hafaw' 
+            },
+            type: "admin"
+          }
+        },
+        body:{
+          old_password: 'apassword',
+          new_password: 'newPassword',
+          username :'hafaw'
+      }
+    }
+
+    const res = {
+      sendStatus: jest.fn(),
+      status: jest.fn(),
+      send: jest.fn()
+    }
+
+
+    jest.spyOn(Admin, 'findOne').mockResolvedValue({_id: req.session.logged_in.user.userID}, 'password');
+    bCrypt.compare.mockResolvedValue(false);
+    res.status.mockImplementation(() => res);
+
+    await adminController.postAdminSettings(req, res);
+    expect(res.status).toHaveBeenCalledWith(403);
+    
+  });
+
+    it("postEmployeeSettings successfully sends a 403 response because employee did not enter correct password", async   () => {
+    
+      const req = {
+        session: {
+          logged_in: {
+            user: {
+              userID: '5f8614a2f796ac1c7e62af94' 
+            },
+            type: "employee"
+          }
+        },
+        body:{
+          current_password: 'apassword',
+          new_password: 'newPassword',
+          employee_id: '5f8614a2f796ac1c7e62af94',
+          fname: 'Juan',
+          lname: 'Carlos',
+          email: 'yanyan@gmail.com',
+          contactNumber: '09123456789',
+        }
+      }
+
+    const res = {
+      sendStatus: jest.fn(),
+      status: jest.fn(),
+      send: jest.fn()
+    }
+
+    jest.spyOn(Employee, 'findOne').mockResolvedValue({_id: req.body.employee_id}, 'password');
+    bCrypt.compare.mockResolvedValue(false);
+    res.status.mockImplementation(() => res);
+
+    await employeeController.postEmployeeSettings(req, res);
+    expect(res.status).toHaveBeenCalledWith(403);
+
+  });
+});
+
+
+
